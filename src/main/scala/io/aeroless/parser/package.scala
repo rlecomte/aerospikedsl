b94efaa229021526e21dad8f632af73ff6fa9ec6
Romain Lecomte <romain.lecomte@tabmo.io>
2017-10-01 20:53:09 +0200

    add parsing exception

--- src/main/scala/io/aeroless/parser/package.scala
+++ src/main/scala/io/aeroless/parser/package.scala
@@ -41,11 +41,19 @@ package object parser {
 
     private val interpreter = new AsAlgebra[Stack] {
       override def get[A](field: String)(next: Dsl[A]): Stack[A] = {
-        case o@AsObject(_) => yolo.runUnsafe(next)(o.get(field).get)
+        case o@AsObject(map) => o.get(field) match {
+          case Some(f) => yolo.runUnsafe(next)(f)
+          case None => throw ParseException(s"$field not present on object with fields ${map.keys.mkString(",")}.")
+        }
+        case e => throw ParseException(s"Try to parse object but $e is not.")
       }
 
       override def at[A](idx: Int)(next: Dsl[A]): Stack[A] = {
-        case arr@AsArray(_) => yolo.runUnsafe(next)(arr.at(idx).get)
+        case arr@AsArray(_) => arr.at(idx) match {
+          case Some(v) => yolo.runUnsafe(next)(v)
+          case None => throw ParseException(s"No value in array on index $idx.")
+        }
+        case e => throw ParseException(s"Try to parse array but $e is not.")
       }
 
       override def opt[A](next: Dsl[A]): Stack[Option[A]] = {
@@ -55,10 +63,12 @@ package object parser {
 
       override def readString: Stack[String] = {
         case AsString(s) => s
+        case e => throw ParseException(s"Try to parse a string value but $e is not.")
       }
 
       override def readLong: Stack[Long] = {
         case AsLong(l) => l
+        case e => throw ParseException(s"Try to parse a long value but $e is not.")
       }
 
       override def readNull: Stack[Unit] = _ => ()
@@ -69,10 +79,12 @@ package object parser {
           arr.foreach(e => builder += yolo.runUnsafe(next).apply(e.value))
           builder.result()
         }
+        case e => throw ParseException(s"Try read an array but $e is not.")
       }
 
       override def readFields[A](next: Dsl[A]): Stack[Map[String, A]] = {
         case AsObject(map) => map.mapValues(e => yolo.runUnsafe(next).apply(e.value))
+        case e => throw ParseException(s"Try read an object but $e is not.")
       }
 
       override def pure[A](x: A): Stack[A] = Applicative[Stack].pure(x)
